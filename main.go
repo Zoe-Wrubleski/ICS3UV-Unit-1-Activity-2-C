70929cd526c1d84ed652cd15bae98fac609b857a
default <zoe.wrubleski@stu.ocsb.ca>
2025-09-29 13:05:39 +0000

    string lateral practice using go

---
+++ main.go
@@ -0,0 +1,24 @@
+/*
+ *@author Zoe Wrubleski
+ *@version 1.0.0
+ *@date 2025-09-29
+ *@file overview This program practices string laterals.
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	
+	//string lateral text
+	fmt.Println("I love Computer Science!!")
+
+	//string lateral text
+	fmt.Println("    I  love Computer Science")
+
+	//string lateral text
+	fmt.Println("I   love   Computer   Science   100%")
+
+	fmt.Println("\nDone.")
+}

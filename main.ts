d0887c97f66d20483a3941f1d8270cba7e40769c
default <zoe.wrubleski@stu.ocsb.ca>
2025-09-29 12:56:11 +0000

    string lateral practice

---
+++ main.ts
@@ -0,0 +1,17 @@
+/**
+ *@author Zoe Wrubleski
+ *@version 1.0.0
+ *@date 2025-09-29
+ *@fileoverview This program practices with string laterals.
+ */
+
+// string lateral text
+console.log("I love Computer Science!!");
+
+//string lateral text
+console.log("    I  love Computer Science");
+
+// string lateral text
+console.log("I   love   Computer   Science   100%");
+
+console.log("\nDone.");
